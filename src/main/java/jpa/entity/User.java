b940e4990f8c5e5ab6a185499bb146e89522dbeb
Nicholas Grynkevych <nicholasg@rambler.ru>
2015-12-17 21:07:37 +0200

    added entity User

---
+++ src/main/java/jpa/entity/User.java
@@ -0,0 +1,55 @@
+package jpa.entity;
+
+
+import javax.persistence.*;
+
+/**
+ * Created by NicholasG on 17.12.2015.
+ */
+@Entity
+public class User {
+    @Id
+    @Column(name = "userID", nullable = false)
+    private int userID;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/jpa/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package jpa.repository;
+
+/**
+ * Created by NicholasG on 17.12.2015.
+ */
+public interface UserRepository {
+}
