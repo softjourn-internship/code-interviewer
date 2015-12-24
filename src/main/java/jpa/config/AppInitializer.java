01c21752abd42162363ad8e56acc75a794513d9d
Iwan <zobenko96@mail.ru>
2015-12-25 00:34:28 +0200

    added DataConfig, AppInitializer and app.properties

---
+++ src/main/java/jpa/config/AppInitializer.java
@@ -0,0 +1,24 @@
+package jpa.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by Iwan on 24.12.2015.
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {
+          DataConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
