bf232116eb6910378a15a10e02dcff5c9236cd86
Iwan <zobenko96@mail.ru>
2016-01-19 19:12:24 +0200

    added connection pool and fixed H2DataConfiguration

--- src/main/java/com/code/reviewer/configuration/H2DataConfiguration.java
+++ src/main/java/com/code/reviewer/configuration/H2DataConfiguration.java
@@ -1,25 +1,35 @@
 package com.code.reviewer.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.stereotype.Component;
+
 
 /**
  * Created by Iwan on 06.01.2016.
  */
+@Component
+@Profile("dev")
+public class H2DataConfiguration implements ApplicationListener<ContextRefreshedEvent> {
 
-@Configuration
-public class H2DataConfiguration {
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("code-interviewer-data-initialization.sql")
+                .build();
 
-    @Bean
-    public EmbeddedDatabase dataSource() {
-        return new EmbeddedDatabaseBuilder().
-                setType(EmbeddedDatabaseType.H2).
-                addScript("create-table.sql").
-                addScript("ref.sql").
-                addScript("code-interviewer-data-initialization.sql").
-                build();
+        ComboPooledDataSource cpds = new ComboPooledDataSource();
+        cpds.setJdbcUrl("jdbc:h2:mem:testdb");
+        cpds.setUser("sa");
+        cpds.setPassword("");
+        cpds.setMaxPoolSize(15);
+        cpds.setMaxIdleTime(100);
+        cpds.setAcquireRetryAttempts(1);
+        cpds.setNumHelperThreads(3);
     }
 }
--- src/main/resources/code-interviewer-data-initialization.sql
+++ src/main/resources/code-interviewer-data-initialization.sql
@@ -1,13 +1,26 @@
-INSERT INTO users VALUES ('admin', 'Mykola', 'Grynkevych', 'koly@mail.com', 'admin', 'ROLE_ADMIN', TRUE, NULL, NULL);
-INSERT INTO users VALUES ('NicholasG', 'Mykola', 'Grynkevych', 'koly@mail.com', '32342', 'ROLE_MANAGER', TRUE, NULL, NULL);
-INSERT INTO users VALUES ('Ivan96A', 'Iwan', 'Arabchuk', 'iwan@gmail.com', '323542', 'ROLE_RECRUITER', TRUE, NULL, NULL);
-INSERT INTO users VALUES ('kutsaniuk', 'Dmytro', 'Kutsaniuk', 'dmytro@gmail.com', 'kutsaniuk', 'ROLE_MANAGER', TRUE, NULL, NULL);
-INSERT INTO users VALUES ('demy4', 'Yuriy', 'Demkiv', 'yuriy@gmail.com', '32342', 'ROLE_ADMIN', TRUE, NULL, NULL);
-INSERT INTO users VALUES ('andrusyakA', 'Vitaliy', 'Andrusyak', 'vitaliy@gmail.com', '32342', 'ROLE_RECRUITER', TRUE, NULL, NULL);
-INSERT INTO users VALUES ('Pushkedra', 'Grugoriy', 'Pushkedra', 'grygoriy@gmail.com', '32342', 'ROLE_MANAGER', FALSE, NULL, NULL);
-INSERT INTO users VALUES ('MulaDmytro', 'Dmytro', 'Mula', 'mula@gmail.com', '32342', 'ROLE_RECRUITER', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('admin', 'Mykola', 'Grynkevych', 'koly@mail.com', 'admin', 'ROLE_ADMIN', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('NicholasG', 'Mykola', 'Grynkevych', 'koly@mail.com', '32342', 'ROLE_MANAGER', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('Ivan96A', 'Iwan', 'Arabchuk', 'iwan@gmail.com', '323542', 'ROLE_RECRUITER', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('kutsaniuk', 'Dmytro', 'Kutsaniuk', 'dmytro@gmail.com', 'kutsaniuk', 'ROLE_MANAGER', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('demy4', 'Yuriy', 'Demkiv', 'yuriy@gmail.com', '32342', 'ROLE_ADMIN', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('andrusyakA', 'Vitaliy', 'Andrusyak', 'vitaliy@gmail.com', '32342', 'ROLE_RECRUITER', TRUE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('Pushkedra', 'Grugoriy', 'Pushkedra', 'grygoriy@gmail.com', '32342', 'ROLE_MANAGER', FALSE, NULL, NULL);
+INSERT INTO users(username, first_name, last_name, email, password, role, active, image, background)
+    VALUES ('MulaDmytro', 'Dmytro', 'Mula', 'mula@gmail.com', '32342', 'ROLE_RECRUITER', TRUE, NULL, NULL);
 
-INSERT INTO participants VALUES (1, 'Ivan', 'Karamov', 'adsdasd@gmail.com', '2015-12-25', '2015-12-26', 'done', TRUE, NULL) ;
-INSERT INTO participants VALUES (2, 'Vasya', 'Bimov', 'fsdfdd@gmail.com', '2015-12-04', '2015-12-05', 'done', TRUE, NULL );
-INSERT INTO participants VALUES (3, 'Misha', 'Hoshiy', 'aggggsd@gmail.com', '2015-12-09', '2015-12-10', 'inProcess', TRUE, NULL) ;
-INSERT INTO participants VALUES (4, 'Ihor', 'Wol', 'allsdasd@gmail.com', '2015-12-06', '2015-12-07', 'inProcess', TRUE, NULL);
+
+INSERT INTO participants(participant_id, first_name, last_name, email, sent, returned, taken, active, image)
+    VALUES (1, 'Ivan', 'Karamov', 'adsdasd@gmail.com', '2015-12-25', '2015-12-26', 'done', TRUE, NULL) ;
+INSERT INTO participants(participant_id, first_name, last_name, email, sent, returned, taken, active, image)
+    VALUES (2, 'Vasya', 'Bimov', 'fsdfdd@gmail.com', '2015-12-04', '2015-12-05', 'done', TRUE, NULL );
+INSERT INTO participants(participant_id, first_name, last_name, email, sent, returned, taken, active, image)
+    VALUES (3, 'Misha', 'Hoshiy', 'aggggsd@gmail.com', '2015-12-09', '2015-12-10', 'inProcess', TRUE, NULL);
+INSERT INTO participants(participant_id, first_name, last_name, email, sent, returned, taken, active, image)
+    VALUES (4, 'Ihor', 'Wol', 'allsdasd@gmail.com', '2015-12-06', '2015-12-07', 'inProcess', TRUE, NULL);
