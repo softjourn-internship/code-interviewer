0e9d0fd8a54760814cdddd0b6adb7cfe1201b9ab
kutsaniuk <kutsaniuk@gmail.com>
2016-01-28 17:18:31 +0300

    Added request method post

--- src/main/webapp/scripts/controllers/Data.js
+++ src/main/webapp/scripts/controllers/Data.js
@@ -11,11 +11,11 @@ adminPanelApp.controller('DataCtrl', ['$scope', '$http', '$location','$routePara
         $http.get(url).success(function(data) {
           $scope.dataTableList = data;
 
-        $scope.titleTable = $scope.dataTable;
+          $scope.titleTable = $scope.dataTable;
 
-        // Sort
-        $scope.sortField = undefined;
-        $scope.reverse = false;
+          // Sort
+          $scope.sortField = undefined;
+          $scope.reverse = false;
 
         $scope.sort = function(fieldName) {
           if ($scope.sortField === fieldName) {
@@ -25,7 +25,7 @@ adminPanelApp.controller('DataCtrl', ['$scope', '$http', '$location','$routePara
             $scope.sortField = fieldName;
             $scope.reverse = false;
           }
-        };
+        }
 
         $scope.isSortUp = function(fieldName) {
           return $scope.sortField === fieldName && !$scope.reverse;
@@ -70,7 +70,7 @@ adminPanelApp.controller('DataCtrl', ['$scope', '$http', '$location','$routePara
           $scope.pageNumber = Math.ceil($scope.lengthClientsPA.length / $scope.pageSize);
           alert($scope.pageNumber+"\n"+$scope.lengthClientsPA.length+"\n"+$scope.pageSize);
       }
-    });
+    
 
     $scope.clearFields = function() {
         $scope.firstName = null;
@@ -78,15 +78,19 @@ adminPanelApp.controller('DataCtrl', ['$scope', '$http', '$location','$routePara
         $scope.city = null;
         $scope.email = null;
       };
-
+      var current_id = null;
       $scope.currentItem = function(itemId, edit) {
         var url = 'api/participants/' + itemId;
 
         $http.get(url).success(function(data) {
           $scope.itemId = data.participantId;
+          current_id = data.participantId;
+          $scope.userName = data.username;
           $scope.firstName = data.firstName;
           $scope.lastName = data.lastName;
           $scope.city = data.city;
+          titleSelect($scope.englishLevel, 'engLevel', false);
+          titleSelect($scope.speciality, 'speciality', false);
           $scope.email = data.email;
           $scope.image = data.image;
         });
@@ -99,38 +103,58 @@ adminPanelApp.controller('DataCtrl', ['$scope', '$http', '$location','$routePara
 
       var text = document.getElementById(id).value;
 
-      if (pattern.test(text) && text !== null) {$('input#' + id).css('border-color','#AEC785'); validation = true;}
+      if (pattern.test(text) && text !== null) {$('input#' + id).css('border-color','#AEC785'); return true;}
       else {
         // $scope.firstName = $scope.firstName.replace(/[0-9]/g, '');
         $('input#' + id).css('border-color','#FA8564');
         $('input#' + id).focus();
-        validation = false;
+        return false;
       }
     }
 
     $scope.validationSelect = function (id, value, failedValue) {
 
-      if (value !== failedValue) {$('span#' + id).css('border-color','#AEC785'); validation = true;}
+      if (value !== failedValue) {$('span#' + id).css('border-color','#AEC785'); return true;}
       else {
         $('span#' + id).css('border-color','#FA8564');
-        validation = false;
+        return false;
       }
     }
 
     $scope.validation = function () {
-      $scope.validationInput('firstName', /^[_a-zA-Zа-яА-Я ]+$/);
-      $scope.validationInput('lastName', /^[_a-zA-Zа-яА-Я ]+$/);
-      $scope.validationInput('city', /^[_a-zA-Zа-яА-Я ]+$/);
-      $scope.validationSelect('selectengLevel', value_engLevel, 'Select engLevel');
-      $scope.validationSelect('selectspeciality', value_speciality, 'Select speciality');
-      $scope.validationInput('email', /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/);
+      var valid_firstName = $scope.validationInput('firstName', /^[_a-zA-Zа-яА-Я ]+$/);
+      var valid_lastName = $scope.validationInput('lastName', /^[_a-zA-Zа-яА-Я ]+$/);
+      var valid_city = $scope.validationInput('city', /^[_a-zA-Zа-яА-Я ]+$/);
+      var valid_value_engLevel = $scope.validationSelect('selectengLevel', value_engLevel, 'Select engLevel');
+      var valid_value_speciality = $scope.validationSelect('selectspeciality', value_speciality, 'Select speciality');
+      var valid_email = $scope.validationInput('email', /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/);
+
+      if(valid_firstName && valid_lastName && valid_city && valid_value_engLevel && valid_value_speciality && valid_email) validation = true;
+      else validation = false;
     }
 
     $scope.sumbit = function () {
       $scope.validation();
-
+      var data;
       if (validation) {
-        alert('Sent');
+        data = {
+          "firstName": $scope.firstName,
+          "lastName": $scope.lastName,
+          "city": $scope.city,
+          "speciality": value_speciality,
+          "englishLevel": value_engLevel,
+          "email": $scope.email,
+          "sent":"2015-12-25","returned":
+          "2015-12-26","taken":"done",
+          "active":true,
+          "image":null,
+          "background":null
+        }
+
+        $http.post("/api/participants", data).success(function (data, status, headers){
+          alert('Sent');
+        });
       }
     }
+    });
   }]);
