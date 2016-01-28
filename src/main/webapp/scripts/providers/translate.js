4c52e499e9df142b802dc99d1d9922eeb036599e
Gregory <phantom1assasin4@gmail.com>
2016-01-29 00:45:28 +0200

    Fix crazy angular-translate

--- src/main/webapp/scripts/providers/translate.js
+++ src/main/webapp/scripts/providers/translate.js
@@ -1,17 +1,15 @@
-adminPanelApp.config(['$translateProvider', function($translateProvider){
+adminPanelApp.config(['$translateProvider', function($translateProvider,$http){
 
   // Register a loader for the static files
   // So, the module will search missing translation tables under the specified urls.
   // Those urls are [prefix][langKey][suffix].
   $translateProvider.useStaticFilesLoader({
-    prefix: '/data/translate/',
+    prefix: '/scripts/providers/',
     suffix: '.json'
   });
 
-  // Tell the module what language to use by default
-
   // Tell the module to store the language in the cookies
-  //
+  //$translateProvider.preferredLanguage('en');
   //$translateProvider.useCookieStorage();
 
 }])
