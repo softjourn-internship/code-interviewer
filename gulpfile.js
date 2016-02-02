5005bb86730b3caeee652157d7b81839c81206bb
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2016-02-02 13:15:37 +0200

    Added gulp && fixed package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
+var src = 'src/main/webapp/';
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	uncss = require('gulp-uncss'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifyCSS = require('gulp-minify-css');
  
-gulp.task('default', function () {
-  return gulp.src('src/**/*.css')
-    .pipe(concatCss("styles/bundle.css"))
-    .pipe(gulp.dest('out/'));
+gulp.task('css', function () {
+  return gulp.src(src + 'styles/*.css')
+    .pipe(concatCss('main.css'))
+    .pipe(autoprefixer('last 5 versions'))
+    .pipe(minifyCSS())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(src + 'dist/styles/'))
+    .pipe(notify('Done!'));
+});
+
+gulp.task('un-css', function () {
+    return gulp.src(src + 'styles/*.css')
+        .pipe(uncss({
+            html: [src + 'index.html', src + 'template/*.html']
+        }))
+        .pipe(gulp.dest(src + 'fix/css'))
+        .pipe(notify('Fixed!'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(src + 'styles/*.css', ['css'])
 });
