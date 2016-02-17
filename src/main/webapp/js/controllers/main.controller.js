340ac9d7af682ea5b765f3d5e09ad54281e26278
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2016-02-17 16:07:54 +0200

    Fixed grid

---
+++ src/main/webapp/js/controllers/main.controller.js
@@ -0,0 +1,21 @@
+app.controller("MainCtrl", function ($scope) {
+	var sidebarVisible = false;
+
+	$scope.sidebarToggle = function() {
+		var sidebar = angular.element(document.querySelector('#sidebarToggle'));
+		if (sidebarVisible) {
+			sidebar.css('left', 0);
+			sidebarVisible = false;
+		}
+		else { sidebar.css('left', '180px'); sidebarVisible = true;}
+	}
+
+	window.onresize = function(event) {
+		var sidebar = angular.element(document.querySelector('#sidebarToggle'));
+    	if(window.innerWidth > 767) {
+			sidebar.css('left', '180px');
+		}
+		else {sidebar.css('left', 0); sidebarVisible = false;}
+	}
+
+});
