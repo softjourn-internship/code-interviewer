f9f82b8a320c500f44482426f6e0e9a10a66a505
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2016-02-23 19:22:36 +0200

    Added a new design workspace

--- src/main/java/com/code/reviewer/Application.java
+++ src/main/java/com/code/reviewer/Application.java
@@ -1,26 +1,26 @@
-package com.code.reviewer;
+    package com.code.reviewer;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.boot.builder.SpringApplicationBuilder;
+    import org.springframework.boot.context.web.SpringBootServletInitializer;
+    import org.springframework.context.annotation.ComponentScan;
 
-/**
- * Created by NicholasG on 18.12.2015.
- */
-@SpringBootApplication
-@ComponentScan
-public class Application extends SpringBootServletInitializer {
+    /**
+     * Created by NicholasG on 18.12.2015.
+     */
+    @SpringBootApplication
+    @ComponentScan
+    public class Application extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            return builder.sources(Application.class);
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+
     }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-}
--- src/main/webapp/modules/profile/profile.controller.js
+++ src/main/webapp/modules/profile/profile.controller.js
@@ -1,6 +1,7 @@
 app.controller('ProfileCtrl',
-	['$scope', '$location', '$http', '$stateParams', 'ngDialog', 'ParticipantService', 'UserService',
 	function ($scope, $location, $http, $stateParams, ngDialog, ParticipantService, UserService) {
+		$scope.MODULE = 'profile';
+
 		var success = function (response) {
 			$scope.account = response.data;
 		}
@@ -13,4 +14,18 @@ app.controller('ProfileCtrl',
 			ngDialog.open({ template: 'modules/dialog/dialog.schedule.template.html'});
 		};
 
-	}]);
+		$scope.doughnut = {
+            labels: ["Java", "HTML", "CSS", "Java Script"],
+            data: [50, 15, 26, 12]
+        }
+
+        $scope.line = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            series: ['Сompleted tests', 'Falied tests'],
+            data: [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+            ]
+        }
+
+	});
